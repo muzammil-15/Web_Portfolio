a4aff0324d493b7e74f3ac73fd853220b662f498
muzammil-15 <muzammilcoder15@gmail.com>
2024-02-10 20:03:48 +0500

    changes in form conponent

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const Form = () => {
-
   const formRef = useRef();
   const [form, setForm] = useState({
     firstName: "",
@@ -30,14 +29,14 @@ const Form = () => {
     e.preventDefault();
     setLoading(true);
 
-    console.log(form)
+    console.log(form);
 
     emailjs
       .send(
         process.env.NEXT_PUBLIC_EMAIL_SERVICE_KEY,
         process.env.NEXT_PUBLIC_EMAIL_TEMPLETE_KEY,
         {
-          from_name: form.firstName+" "+form.lastName,
+          from_name: form.firstName + " " + form.lastName,
           to_name: "Muzammil Hussain",
           from_phone: form.phone,
           from_email: form.email,
@@ -52,14 +51,16 @@ const Form = () => {
           alert("Thank you. I will get back to you as soon as possible.");
 
           setForm({
-            name: "",
+            firstName: "",
+            lastName: "",
+            phone: null,
             email: "",
             message: "",
           });
         },
         (error) => {
           setLoading(false);
-          console.error(error);
+          console.error("error", error);
 
           alert("Ahh, something went wrong. Please try again.");
         }
@@ -67,10 +68,7 @@ const Form = () => {
   };
   return (
     <div className="my-16 w-full md:w-[90%] lg:w-[80%]">
-      <form
-        ref={formRef}
-        onSubmit={handleSubmit}
-      >
+      <form ref={formRef} onSubmit={handleSubmit}>
         <div className="w-full flex flex-col gap-4 md:flex-row justify-center items-center md:gap-10">
           <div className="flex flex-col gap-1 w-full md:w-1/2">
             <label htmlFor="fname" className="text-lg font-[500] text-[#FFF]">
@@ -80,7 +78,7 @@ const Form = () => {
               type="text"
               className="p-3 bg-transparent border border-[#8d8c8cb2] rounded-lg w-full focus:bg-transparent"
               placeholder="First Name"
-              name='firstName'
+              name="firstName"
               value={form.firstName}
               onChange={handleChange}
             />
@@ -93,7 +91,7 @@ const Form = () => {
               type="text"
               className="p-3 bg-transparent border border-[#8d8c8cb2] rounded-lg w-full focus:bg-transparent"
               placeholder="Last Name"
-              name='lastName'
+              name="lastName"
               value={form.lastName}
               onChange={handleChange}
             />
@@ -108,7 +106,7 @@ const Form = () => {
               type="number"
               className="p-3 bg-transparent border border-[#8d8c8cb2] rounded-lg w-full focus:bg-transparent"
               placeholder="Phone"
-              name='phone'
+              name="phone"
               value={form.phone}
               onChange={handleChange}
             />
@@ -121,7 +119,7 @@ const Form = () => {
               type="email"
               className="p-3 bg-transparent border border-[#8d8c8cb2] rounded-lg w-full focus:bg-transparent"
               placeholder="Email"
-              name='email'
+              name="email"
               value={form.email}
               onChange={handleChange}
             />
@@ -137,15 +135,18 @@ const Form = () => {
               rows={8}
               className="p-3 bg-transparent border border-[#8d8c8cb2] rounded-md w-full focus:bg-transparent"
               placeholder="Enter Your Valuable message"
-              name='message'
+              name="message"
               value={form.message}
               onChange={handleChange}
             />
           </div>
         </div>
         <div className="flex justify-center items-center my-6">
-          <button type="submit" className="w-[150px] flex justify-center items-center py-3 px-6 rounded-full bg-button text-[#F4F7FA] text-sm">
-          {loading ? "Sending..." : "Send"}
+          <button
+            type="submit"
+            className="w-[150px] flex justify-center items-center py-3 px-6 rounded-full bg-button text-[#F4F7FA] text-sm"
+          >
+            {loading ? "Sending..." : "Send"}
           </button>
         </div>
       </form>
