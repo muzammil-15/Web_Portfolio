2f9fbb33eaaa1aaed73278f58097e59d427710d8
muzammil-15 <muzammilcoder15@gmail.com>
2024-02-10 20:50:29 +0500

    changes in sparkles compoent

--- src/components/SparklesComponent.jsx
+++ src/components/SparklesComponent.jsx
@@ -9,11 +9,11 @@ export function SparklesPreview({children}) {
       <h1 className="md:text-5xl text-2xl lg:text-7xl font-bold text-center text-white relative z-20">
         {children}
       </h1>
-      <div className="w-[40rem] h-10 relative">
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+      <div className="w-full md:w-[40rem] h-10 relative overflow-hidden">
+        <div className="absolute inset-x-0 md:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-full sm:w-3/4 blur-sm" />
+        <div className="absolute inset-x-0 md:inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-full sm:w-3/4" />
+        <div className="absolute inset-x-20 md:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+        <div className="absolute inset-x-20 md:inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
 
         
 
